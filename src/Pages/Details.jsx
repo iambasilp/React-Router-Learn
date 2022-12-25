91afd48ee9b20a7001cf014ea20500dadce25c08
iambasilp <basilxg321@gmail.com>
2022-12-25 19:31:05 +0530

    some solve

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -4,29 +4,39 @@ import { useParams, useNavigate, useLocation } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 import "./Styles/Details.css";
 export const Details = () => {
+  const { userId } = useParams();
+  const location = useLocation();
+  const state = location.state || {};
+  const [user, setUser] = useState(state.data);
+  const navigate = useNavigate();
 
-const { userId } = useParams();
-const location = useLocation()
-const state = location.state || {}
-const [user, setUser] = useState(state.data);
-const navigate = useNavigate();
-   
   useEffect(() => {
-    if(!user){
-            axios("/data.json").then((response) => {
-      const data = response.data.find((item) => item.id == parseInt(userId));
-      setUser(data);
-    });
+    if (!user) {
+      axios("/data.json").then((response) => {
+        const item = response.data.find((item) => item.id == userId);
+        setUser(item);
+      });
     }
-
-  }, [userId,user]);
+  }, [userId, user]);
   console.log(user);
   return (
     <div className="details">
-      <div className="back" onClick={() => {
-        navigate('/Users')
-      }}>
-       <svg className="back-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 13v7l-8-8 8-8v7h8v2z"/></svg>
+      <div
+        className="back"
+        onClick={() => {
+          navigate("/Users");
+        }}
+      >
+        <svg
+          className="back-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path d="M12 13v7l-8-8 8-8v7h8v2z" />
+        </svg>
       </div>
       <div className="details_card">
         <span className="details_roll">00{user.id}</span>
