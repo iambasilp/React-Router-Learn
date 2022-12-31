5211ef34981e73b13485fac95627ac5cbba9cd37
iambasilp <basilxg321@gmail.com>
2022-12-31 15:53:12 +0530

    some css added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom'
+import { navToggle } from '../Utils/common'
+import { navClose } from '../Utils/common'
+import { navLink } from '../Utils/common'
 import LoggedInOutlet from './Outlets/LoggedInOutlet'
 import { clearUserInfo } from '../Utils/common'
 import './Header.css'
+import 'remixicon/fonts/remixicon.css'
 const Header = () => {
   return (
     <>
     <header className="header">
         <nav className="nav container">
             <span to="/Home" className="nav__logo">Basil.</span>
-            <div className="nav__menu">
+            <div className="nav__menu" id='nav-menu'>
                 <ul className="nav__list">
                     <li className="nav__item">
-                        <NavLink to="Home" className="nav__Link">Home</NavLink>
-                        <NavLink to="About" className="nav__Link">About</NavLink>
-                        <NavLink to="Users" className="nav__Link">Users</NavLink>
-                        <NavLink to="Contact" className="nav__Link">Contact</NavLink>     
-                        <NavLink to="Login" style={{color:"red"}} className="nav__Link" onClick={()=>{clearUserInfo()}}>Logout</NavLink>     
+                        <NavLink to="Home" className="nav__Link" onClick={()=>{
+                            navLink()
+                        }}>Home</NavLink>
+                        <NavLink to="About" className="nav__Link"
+                        onClick={()=>{
+                            navLink()
+                        }}>About</NavLink>
+                        <NavLink to="Users" className="nav__Link"
+                        onClick={()=>{
+                            navLink()
+                        }}>Users</NavLink>
+                        <NavLink to="Contact" className="nav__Link"
+                        onClick={()=>{
+                            navLink()
+                        }}>Contact</NavLink>     
+                        <NavLink to="Login" className="nav__Link"  onClick={()=>{clearUserInfo()}}>Logout</NavLink>     
                     </li>
                 </ul>
+                <div className="nav__close" id='nav-close' onClick={()=>{
+                    navClose()
+                }} >
+                    <i className="ri-close-line"></i>
+                </div>
+            </div>
+            <div className="nav__toggle" id='nav-toggle' >
+            <i className="ri-menu-line" onClick={()=>{
+                navToggle()
+            }}></i>
             </div>
         </nav>
     </header>
--- src/Utils/common.js
+++ src/Utils/common.js
@@ -1,15 +1,32 @@
-const saveUserInfo = (user)=>{
-    localStorage.setItem('cred',JSON.stringify(user))
+const saveUserInfo = (user) => {
+    localStorage.setItem('cred', JSON.stringify(user))
 }
-const clearUserInfo = ()=>{
+const clearUserInfo = () => {
     localStorage.removeItem('cred')
 }
-const isLoggedin = ()=>{
-    try{
+const isLoggedin = () => {
+    try {
         const Value = JSON.parse(localStorage.getItem('cred'))
         return !!Value
-    }catch{
+    } catch {
         return false
     }
 }
-export {saveUserInfo, isLoggedin, clearUserInfo}
+
+// toggle
+const navToggle = () => {
+    const Menu = document.querySelector('#nav-menu')
+    Menu.classList.add('show-menu')
+}
+const navClose = () => {
+    const Menu = document.querySelector('#nav-menu')
+    Menu.classList.remove('show-menu')
+ 
+}
+const navLink = () => {
+    const Menu = document.querySelector('#nav-menu')
+    Menu.classList.remove('show-menu')
+}
+
+
+export { saveUserInfo, isLoggedin, clearUserInfo,navToggle,navClose,navLink}
